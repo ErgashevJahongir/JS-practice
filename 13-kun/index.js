1668a2ecd22e1bcc8878f5dd89c9b982e483c8e3
JohaErgashev <91261401+JohaErgashev@users.noreply.github.com>
2021-11-28 21:44:57 +0500

    13-kun Slider qilish

---
+++ 13-kun/index.js
@@ -0,0 +1,34 @@
+// let a = prompt("son kiriting: ");
+
+// function tubBuluvchilar(a) {
+//     if (a <= 2) {
+//         console.log("2 dan katta son kiriting");
+//     } else {
+//         let g = 0;
+//         for (let i = 2; i < a; i++) {
+//             if (a % i == 0) {
+//                 let k = 0;
+//                 for (let j = 1; j <= i; j++) {
+//                     if (i % j == 0) {
+//                         if (i == 2) {
+//                             console.log("tub bo'luvchi " + i);
+//                             break;
+//                         }
+//                     } else {
+//                         k++;
+//                         if (i - 2 == k) {
+//                             console.log("tub bo'luvchi " + i);
+//                         }
+//                     }
+//                 }
+//             } else {
+//                 g++;
+//                 if (a - 2 == g) {
+//                     console.log("siz tub son kiritdingiz " + a);
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// tubBuluvchilar(a);
